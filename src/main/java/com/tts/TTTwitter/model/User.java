58b00b27c4e766b12458ca7062747c9d1e7f9a64
David Adams <david.riding.rails@gmail.com>
2019-06-28 21:51:28 -0700

    set up User model

--- src/main/java/com/tts/TTTwitter/model/User.java
+++ src/main/java/com/tts/TTTwitter/model/User.java
@@ -1,8 +1,91 @@
 package com.tts.TTTwitter.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  @Column(name="user_id")
+  private Long id;
+  private String email;
+  private String username;
+  private String password;
+  private String firstName;
+  private String lastName;
+  private Integer active;
+  
+  public User() {};
+  
+  public User(String email, String username, String password, String firstName, String lastName, Integer active) {
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.active = active;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public Integer getActive() {
+    return active;
+  }
+
+  public void setActive(Integer active) {
+    this.active = active;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  @Override
+  public String toString() {
+    return "User [id=" + id + ", email=" + email + ", username=" + username + ", password=" + password + ", firstName="
+        + firstName + ", lastName=" + lastName + ", active=" + active + "]";
+  }
   
 }

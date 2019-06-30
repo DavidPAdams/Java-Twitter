c0b1d5a2663210280504198cb691bf43401924e2
David Adams <david.riding.rails@gmail.com>
2019-06-29 18:10:55 -0700

    mod UserController

--- src/main/java/com/tts/TTTwitter/controller/UserController.java
+++ src/main/java/com/tts/TTTwitter/controller/UserController.java
@@ -27,6 +27,17 @@ public class UserController {
    public String getUser(@PathVariable("username") String username, Model model) {
      User user = userService.findByUsernameContainsIgnoreCase(username);
      List<Tweet> tweets = tweetService.findAllByUser(user);
+     User loggedInUser = userService.getLoggedInUser();
+     List<User> following = loggedInUser.getFollowing();
+     boolean isFollowing = false;
+     for (User followedUser : following) {
+       if (followedUser.getUsername().equals(username)) {
+         isFollowing = true;
+       }
+     }
+     boolean isSelfPage = loggedInUser.getUsername().equals(username);
+     model.addAttribute("isSelfPage", isSelfPage);
+     model.addAttribute("following", isFollowing);
      model.addAttribute("tweetList", tweets);
      model.addAttribute("user", user);
      return "user";
@@ -35,9 +46,25 @@ public class UserController {
    @GetMapping(value = "/users")
    public String getUsers(Model model) {
      List<User> users = userService.findAll();
+     User loggedInUser = userService.getLoggedInUser();
+     List<User> usersFollowing = loggedInUser.getFollowing();
+     SetFollowingStatus(users, usersFollowing, model);
      model.addAttribute("users", users);
      SetTweetCounts(users, model);
      return "users";
+   }
+   
+   private void SetFollowingStatus(List<User> users, List<User> usersFollowing, Model model) {
+     HashMap<String,Boolean> followingStatus = new HashMap<String, Boolean>();
+     String username = userService.getLoggedInUser().getUsername();
+     for (User user : users) {
+       if(usersFollowing.contains(user)) {
+         followingStatus.put(user.getUsername(), true);
+       }else if (!user.getUsername().equals(username)) {
+         followingStatus.put(user.getUsername(), false);
+       }
+     }
+   model.addAttribute("followingStatus", followingStatus);
    }
    
    private void SetTweetCounts(List<User> users, Model model) {
